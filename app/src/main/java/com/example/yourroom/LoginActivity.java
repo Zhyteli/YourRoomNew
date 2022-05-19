5bd6687fb4a067f45555f81108ed50b5ab8a8107
Asus <kirill.zhikhor15@gmail.com>
2022-05-19 13:56:17 +0300

    Создание навигации

---
+++ app/src/main/java/com/example/yourroom/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.yourroom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        login = findViewById(R.id.btSignIn);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
