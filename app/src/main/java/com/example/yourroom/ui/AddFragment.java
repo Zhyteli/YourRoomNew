ede092c46ae8fd2c573a4e4f83abb463de7dfb99
Asus <kirill.zhikhor15@gmail.com>
2022-05-20 15:48:09 +0300

    Визуал добовления в fragment_add.xml

--- app/src/main/java/com/example/yourroom/ui/AddFragment.java
+++ app/src/main/java/com/example/yourroom/ui/AddFragment.java
@@ -7,17 +7,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import com.example.yourroom.R;
 
 
 public class AddFragment extends Fragment {
-
-
+    private EditText mEditTextFileAddress, mEditTextFilePrice, mEditTextFileDescription,
+            mEditTextFileEmail, mEditTextFilePhone;
+    private RadioButton mRadioButtonRent, mRadioButtonDaily, mRadioButtonResidential, mRadioButtonNon_residential;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add, container, false);
     }
+
 }
